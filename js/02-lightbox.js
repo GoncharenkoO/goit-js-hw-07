127ad78abb0614d0ecea9e9244eb3d52d459120e
Olga Goncharenko <goncharenko86@gmail.com>
2022-01-13 15:18:54 +0200

    Initial commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.gallery');
+
+// function selectorGallery() {
+//     const markup = galleryItems.map(({ preview, original, description }) => {
+//         return `<a class="gallery__item" href=${original}><img class= "gallery__image" src="${preview}" alt="" title="${description}"></a>`
+//     }).join('');
+   
+//     galleryItemsEl.insertAdjacentHTML("afterbegin", markup);
+// }
+// selectorGallery();
+
+
+const selectorGallery = (images) => {
+    return images.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href=${original}><img class= "gallery__image" src="${preview}" alt="" title="${description}"></a>`
+    }).join('');
+}
+
+const cardImagesMarkup = selectorGallery(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("afterbegin", cardImagesMarkup);
+
+galleryItemsEl.addEventListener('click', selectGalleryEl);
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+function selectGalleryEl(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    lightbox;
+}
